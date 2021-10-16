bb7c511e17f6b3344f29cfc25f0edbb70482b57c
Alexis Oswald <alexisoswald98@gmail.com>
2021-10-15 22:38:25 -0400

    Adding base calendar component.

--- src/app/calendar/calendar.component.ts
+++ src/app/calendar/calendar.component.ts
@@ -1,15 +1,52 @@
-import { Component, OnInit } from '@angular/core';
+//ref: https://stackblitz.com/edit/ng-min-calendar?file=src%2Fapp%2Fcalendar%2Fcalendar.component.ts
+import { Component, EventEmitter, Output} from '@angular/core';
+const DAY_MS = 60 * 60 * 24 * 1000; //days to milliseconds
 
 @Component({
   selector: 'calendar',
   templateUrl: './calendar.component.html',
   styleUrls: ['./calendar.component.scss']
 })
-export class CalendarComponent implements OnInit {
+export class CalendarComponent{
+  dates: Array<Date>;
+  days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+  date = new Date();
+  
+  @Output() selected = new EventEmitter();
 
-  constructor() { }
+  constructor() { 
+    this.dates = this.getCalendarDays(this.date);
+  }
 
-  ngOnInit(): void {
+  setMonth(inc:number) {
+    const [year, month] = [this.date.getFullYear(), this.date.getMonth()];
+    this.date = new Date(year, month + inc, 1);
+    this.dates = this.getCalendarDays(this.date);
+  }
+  
+  isSameMonth(date:Date) {
+    return date.getMonth() === this.date.getMonth();
+  }
+
+  private getCalendarDays(date = new Date) {
+    const calendarStartDate = this.getCalendarStartDay(date) ?? new Date() ;
+    const calendarStartTime = calendarStartDate.getTime();
+
+    return this.range(0, 41)
+      .map(num => new Date(calendarStartTime + DAY_MS * num));
+  }
+
+  private getCalendarStartDay(date = new Date) {
+    const [year, month] = [date.getFullYear(), date.getMonth()];
+    const firstDayOfMonth = new Date(year, month, 1).getTime();
+
+    return this.range(1,7)
+      .map(num => new Date(firstDayOfMonth - DAY_MS * num))
+      .find(dt => dt.getDay() === 0)
+  }
+
+  private range(start:number, end:number, length = end - start + 1) {
+    return Array.from({ length }, (_, i) => start + i)
   }
 
 }
