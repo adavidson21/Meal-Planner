d11cdae3d368116f77347475845a8d09b197782b
Alexis Oswald <alexisoswald98@gmail.com>
2021-12-07 22:39:48 -0500

    Added baseline expansion panel for view-recipes

--- src/app/view-recipes/view-recipes.component.ts
+++ src/app/view-recipes/view-recipes.component.ts
@@ -1,22 +1,20 @@
 import { Component, OnInit } from '@angular/core';
-import { HttpClient,HttpErrorResponse } from "@angular/common/http";
-
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-view-recipes',
   templateUrl: './view-recipes.component.html',
-  styleUrls: ['./view-recipes.component.scss']
+  styleUrls: ['./view-recipes.component.scss'],
 })
 export class ViewRecipesComponent implements OnInit {
-  recipeData:any;
+  recipeData: any;
+  panelOpenState = false;
 
-  constructor(private httpClient: HttpClient){
+  constructor(private httpClient: HttpClient) {}
 
-  }
-
-  ngOnInit(){
-    this.httpClient.get<any>("assets/recipes.json").subscribe((data)=>
-      this.recipeData = data
-    )
+  ngOnInit() {
+    this.httpClient
+      .get<any>('assets/recipes.json')
+      .subscribe((data) => (this.recipeData = data));
   }
 }
