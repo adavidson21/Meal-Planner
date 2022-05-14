dbdb9b1609a866d1e7e2022bc9d4d178d672809a
Alexis Oswald <alexisoswald98@gmail.com>
2022-05-14 12:23:37 -0400

    Add new component routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,15 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AddRecipeComponent } from './add-recipe/add-recipe.component';
 import { ViewRecipesComponent } from './view-recipes/view-recipes.component';
+import { IngredientsListComponent } from './ingredients-list/ingredients-list.component';
+import { MealPlanningComponent } from './meal-planning/meal-planning.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/meals', pathMatch: 'full' },
   { path: 'meals', component: ViewRecipesComponent },
   { path: 'add', component: AddRecipeComponent },
+  { path: 'list', component: IngredientsListComponent },
+  { path: 'planning', component: MealPlanningComponent },
 ];
 
 @NgModule({
